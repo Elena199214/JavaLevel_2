745d4c3acce6a9fadc9e260b4cf0a8bcd2325a10
Gudkova Lena <gudkova_elena1992@mail.ru>
2021-04-25 22:33:50 +0300

    lesson_1

---
+++ src/main/java/Lesson_1/User.java
@@ -0,0 +1,21 @@
+package Lesson_1;
+public  class User {
+
+private static int count = 0;
+protected  String name;
+
+private static final int LENGTH = 200;
+
+public User(String name) {
+        count++;
+        this.name = name;
+        }
+
+public User() {
+        count++;
+        }
+
+public String getName() {
+        return name;
+        }
+        }
