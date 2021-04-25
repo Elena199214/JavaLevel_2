4fd2cfe85cdd30772d46a3009a03d6a2c9f5a2bf
Gudkova Lena <gudkova_elena1992@mail.ru>
2021-04-25 22:33:50 +0300

    lesson_1

---
+++ src/main/java/Lesson_1/Person.java
@@ -0,0 +1,25 @@
+package Lesson_1;
+
+public class Person implements Workable, Eatable
+{
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void work() {
+        System.out.println("Person works");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Person eats");
+    }
+
+    @Override
+    public void rest() {
+        System.out.println("Person rest");
+    }
+}
